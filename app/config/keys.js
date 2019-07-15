2e5958b1cd163048af09906063b45f17d3a6af61
bnonni <bryanwnonni@gmail.com>
2019-07-15 18:16:16 -0400

    remvoe keys

---
+++ app/config/keys.js
@@ -0,0 +1,8 @@
+var username = "";
+var password = "";
+
+module.exports = {
+ MongoURI: "mongodb+srv://" + username + ":" + password + "@jediadventures0-srodk.mongodb.net/jedi_adventures?retryWrites=true&w=majority",
+ secretOrKey: "secret"
+};
+
