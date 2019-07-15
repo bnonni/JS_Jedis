ae0290235e123a69c4d8506bc4ba6fbcbff3ce6c
Dre <deandre.va428@gmail.com>
2019-07-15 18:03:35 -0400

    made chnages

--- app/app.js
+++ app/app.js
@@ -48,3 +48,68 @@ app.use((err, req, res, next) => {
 });
 
 module.exports = app;
+
+app.post('/register', function(req,res){ 
+  var register_password = req.body.register_password; 
+  var register_lastname =req.body.register_lastname; 
+  var register_username =req.body.register_username; 
+  var register_firstname= req.body.register_firstname;
+
+  var data = { 
+      "register_password":password, 
+      "register_lastname":lastname, 
+      "register_username":username, 
+      "register_firstname":firstname,
+      
+  } 
+  
+db.collection('details').insertOne(data,function(err, collection){ 
+      if (err) throw err; 
+      console.log("Record inserted Successfully"); 
+            
+  }); 
+        
+  return res.redirect('signup_success.html'); 
+}) 
+
+
+app.get('/',function(req,res){ 
+res.set({ 
+  'Access-control-Allow-Origin': '*'
+  }); 
+return res.redirect('index.html'); 
+}).listen(3000) 
+
+
+console.log("server listening at port 3000"); 
+
+
+app.post('/login', function(req,res){ 
+  var login_username = form.login_username.value,
+  var login_password = form.login_password.value;
+  
+  var data = { 
+      "register_password":password, 
+      "register_username":username, 
+      
+  } 
+  
+db.collection('details').insertOne(data,function(err, collection){ 
+      if (err) throw err; 
+      console.log("Record inserted Successfully"); 
+            
+  }); 
+        
+  return res.redirect('signup_success.html'); 
+}) 
+
+
+app.get('/',function(req,res){ 
+res.set({ 
+  'Access-control-Allow-Origin': '*'
+  }); 
+return res.redirect('index.html'); 
+}).listen(3000) 
+
+
+console.log("server listening at port 3000"); 
--- app/config/keys.js
+++ app/config/keys.js
@@ -1,5 +1,5 @@
-var username = process.env.MongoUsername;
-var password = process.env.MongoPassword;
+var username = "Deandre";
+var password = "ygd0bkKZLU0c2S2e";
 
 module.exports = {
  MongoURI: "mongodb+srv://" + username + ":" + password + "@jediadventures0-srodk.mongodb.net/jedi_adventures?retryWrites=true&w=majority",
