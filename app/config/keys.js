775a0bd21c3124015d8837f5cfbc6990b071257d
bnonni <bryanwnonni@gmail.com>
2019-07-15 18:16:07 -0400

    remove keys

--- app/config/keys.js
+++
@@ -1,8 +0,0 @@
-var username = "Deandre";
-var password = "ygd0bkKZLU0c2S2e";
-
-module.exports = {
- MongoURI: "mongodb+srv://" + username + ":" + password + "@jediadventures0-srodk.mongodb.net/jedi_adventures?retryWrites=true&w=majority",
- secretOrKey: "secret"
-};
-
