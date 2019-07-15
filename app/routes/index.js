f0d69f868eb4637d154b934a569c329b5e749a70
bnonni <bryanwnonni@gmail.com>
2019-07-15 18:13:26 -0400

    POST user registration

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  res.set({
+    'Access-control-Allow-Origin': '*'
+  });
   res.render('index', { title: 'Jedi Adventures' });
 });
 
--- app/routes/register.js
+++ app/routes/register.js
@@ -2,8 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
- res.render('register', { title: '' });
+router.get('/register', function (req, res, next) {
+ res.post('/register', function (req, res) {
+  var register_password = req.body.register_password;
+  var register_lastname = req.body.register_lastname;
+  var register_username = req.body.register_username;
+  var register_firstname = req.body.register_firstname;
+
+  var data = {
+   "register_password": password,
+   "register_lastname": lastname,
+   "register_username": username,
+   "register_firstname": firstname,
+  }
+
+  db.collection('users').insertOne(data, function (err, collection) {
+   if (err) throw err;
+   console.log("Record inserted Successfully!");
+  });
+
+  return res.redirect('index.html');
+ })
 });
 
 module.exports = router;
