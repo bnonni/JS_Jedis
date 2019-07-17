e64d6cbe47b3d82be1837360e031d895f42a595b
bnonni <bryanwnonni@gmail.com>
2019-07-17 02:47:16 -0400

    lots of changes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,68 +1,74 @@
-var express = require('express'); //require middleware
+var express = require("express"); //require middleware
 var router = express.Router(); //import router
-const User = require('../models/user'); //require mongoose connection & model
+const User = require("../models/user"); //require mongoose connection & model
 
 /* Get home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-router.get('/login', (req, res) => {
-  res.render('login');
-  var username = req.body.reg_username;
-  var user_pass = req.body.reg_password
+router.get("/login/game", (req, res) => {
+  res.render("game");
+});
 
-  User.get('users').findOne({ "username": username }, (err, user) => {
-    err = { error: "Login failed!", message: "Please register for an account!" };
-    if (err) {
-      req.flash('fail_message', err);
-      res.redirect('login', err)
-    } else {
-      user.comparePassword(user_pass, function (err, isMatch) {
-        if (err) {
-          err.message = "Password Invalid!";
-          req.flash('fail_message', err);
-          res.redirect('login', err);
-        }
-        else {
-          req.flash("success_msg", "Valid credentials, logging in. Please wait!");
-          res.redirect("dashboard");
-        }
-      });
-    }
-  });
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
 router.get("/register", (req, res) => {
   res.render("login");
 });
 
-router.post('/register', (req, res) => {
+router.get("/leaderboard", (req, res) => {
+  res.render("leaderboard");
+});
+
+router.post("/login", (req, res) => {
+  var username = req.body.log_username;
+  var userpass = req.body.log_password
+
+  var login = {
+    uname: username,
+    pass: userpass
+  }
+  console.log(login);
+
+  User.findOne({ "username": username }, (err, user) => {
+    if (user == null) {
+      console.log(err);
+      res.render("login", { login_fail_msg: "Login failed! Username not found. Please register for an account!" });
+    } else if (userpass != user.password) {
+      console.log(user);
+      res.render("login", { login_fail_msg: "Password Invalid! Please try again." });
+    }
+    else if ((!err) && (userpass == user.password)) {
+      console.log(user);
+      res.redirect(302, "/login/game");
+    }
+  })
+});
+
+router.post("/register", (req, res) => {
   // Get our form values
-  var first = req.body.firstname;
-  var last = req.body.lastname;
+  var name = req.body.name;
   var username = req.body.reg_username;
   var email = req.body.email;
-  var password = req.body.reg_password;
+  var password = req.body.reg_password2;
 
   //Create new mongoose schema
-  var user = new User({ first, last, email, username, password });
+  var user = new User({ name, email, username, password });
 
   //Save user to db
   user.save()
     .then(item => {
-      req.flash('success_msg', 'You are registered and can now log in!');
-      res.redirect("login");
+      res.render("login");
       console.log(item + " saved to Database!")
     })
     .catch(err => {
-      res.redirect("error");
+      var error = "Registration failed! Please try again!";
+      res.render("login", { register_fail_msg: error });
       console.log("Error saving to database " + err);
     });
-});
-
-router.get("/dashboard", (req, res) => {
-  res.render("dashboard");
 });
 
 
